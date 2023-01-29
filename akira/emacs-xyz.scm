d6eaadbb79d4318676aafa7a2e97d207426d2ee8
Akira Kyle <akira@akirakyle.com>
2023-01-29 14:29:11 -0700

    Add emacs-consult-flyspell and emacs-aas packages

--- akira/emacs-xyz.scm
+++ akira/emacs-xyz.scm
@@ -6,8 +6,8 @@
   #:use-module (guix build-system emacs)
   #:use-module (gnu packages mail)
   #:use-module (gnu packages emacs-xyz)
-  #:use-module (emacs-latest emacs-xyz)
-  #:use-module (emacs-latest utils))
+  #:use-module (emacs-latest utils)
+  #:use-module (emacs-latest emacs-xyz))
 
 ;; TODO upstream this
 (define-public emacs-org-pdftools
@@ -36,6 +36,52 @@
 control.  https://github.com/fuxialexander/org-pdftools/")
     (license license:gpl3+)))
 
+(define-public emacs-consult-flyspell
+  (package
+    (name "emacs-consult-flyspell")
+    (version "HEAD")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://gitlab.com/OlMon/consult-flyspell.git")
+                    (commit "d587961ef0d5f9992cedef3b35b87b11d610a375")))
+              (sha256
+               (base32
+                "0sz64k5fb0jpm2cmynfnnsslb1ka86zq1fdvc5ipbysiqnplfjpb"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-consult))
+    (home-page "https://gitlab.com/OlMon/consult-flyspell")
+    (synopsis "A package to incorporate flyspell into consult. ")
+    (description
+     "A package to incorporate flyspell into consult.
+  This allows to choose a misspelled word, jump to it and optionally
+  apply a function to it.")
+    (license license:gpl3+)))
+
+
+(define-public emacs-aas
+  (package
+    (name "emacs-aas")
+    (version "HEAD")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/ymarco/auto-activating-snippets.git")
+                    (commit "e92b5cffa4e87c221c24f3e72ae33959e1ec2b68")))
+              (sha256
+               (base32
+                "1nl7wm4l30hjcbqrvdci66aa6ax32ih46n58q3imc46z8c6rhqxh"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/ymarco/auto-activating-snippets")
+    (synopsis " Snippets for Emacs that expand as you type")
+    (description
+     "This package implements an engine for auto-expanding snippets. It is done
+by tracking your inputted chars along a tree until you complete a registered key
+sequence. Its like running a long prefix command, but the keys you type are not
+‘consumed’ and appear in the buffer until you complete the whole command - and
+then the snippet is triggered!")
+    (license license:gpl3+)))
+
 (define-public mu-latest
   (package-commit mu
                   "c23dad70586bbb54891c506629f2ce2ed8e463d2"
@@ -55,22 +101,68 @@ control.  https://github.com/fuxialexander/org-pdftools/")
                          (delete "emacs-company")
                          (delete "emacs-markdown-mode")))))
 
+;; TODO: add my patches for emacs-org
 
 (define-public %all-my-emacs-packages
-  (map with-emacs-xyz-latest
-    (append 
-     (map emacs-xyz-latest
-       (list
-        emacs-vterm
-        ;;emacs-pdf-tools
-        emacs-org-roam
-        emacs-citar-org-roam
-        emacs-all-the-icons-completion
-        emacs-magit
-        emacs-geiser-guile
-        ))
-     (list
-      my-emacs-jupyter
-      emacs-org-pdftools
-      mu-latest
-      ))))
+  ;(append 
+   ;(map emacs-xyz-latest
+   (map with-emacs-xyz-latest
+        (list
+         emacs-use-package
+         emacs-doom-themes
+         emacs-doom-modeline
+         emacs-general
+         emacs-which-key
+         emacs-undo-tree
+         emacs-evil
+         emacs-evil-collection
+         emacs-evil-matchit
+         emacs-evil-surround
+         emacs-evil-tex
+         emacs-evil-org
+         emacs-avy
+         emacs-ace-link
+         emacs-ace-window
+         emacs-vertico
+         emacs-orderless
+         emacs-consult
+         emacs-consult-flyspell
+         emacs-flyspell-correct
+         emacs-consult-eglot
+         emacs-marginalia
+         emacs-embark
+         emacs-corfu
+         emacs-cape
+         emacs-tempel
+         emacs-kind-icon
+         emacs-rainbow-delimiters
+         emacs-smartparens
+         emacs-aas
+         emacs-org
+         emacs-org-roam
+         emacs-org-contrib
+         emacs-htmlize
+         emacs-citeproc-el
+         emacs-citar
+         emacs-citar-org-roam
+         emacs-all-the-icons-completion
+         emacs-direnv
+         emacs-pass
+         emacs-vterm
+         emacs-pdf-tools
+         emacs-magit
+         emacs-auctex
+         emacs-geiser-guile
+         emacs-julia-mode
+         emacs-haskell-mode
+         emacs-markdown-mode
+         emacs-json-mode
+         emacs-gcmh
+         ;))
+   ;(map with-emacs-xyz-latest
+        ;(list
+         my-emacs-jupyter
+         emacs-org-pdftools
+         mu-latest
+         )))
+         ;))))
